4caf0622589130a715a68199b02e9dbdc051f762
suhyeon1740 <suhyeon@treenod.com>
2021-12-02 19:19:31 +0900

    list 클릭 시 뷰 화면으로 이동

--- js/index.js
+++ js/index.js
@@ -14,13 +14,21 @@ const posts = [
 ]
 
 const board = document.querySelector("#board")
+
 getBoard()
+
+board.addEventListener("click", (e) => {
+  if (e.target.tagName == "TD") {
+    const link = e.target.parentNode.dataset.link
+    location.href = link
+  }
+})
 
 function getBoard() {
   board.innerHTML = posts
     .map(
       (post) =>
-        `<tr>
+        `<tr data-link="/view.html?user=${post.user}&number=${post.number}">
           <td>${post.number}</td>
           <td>${post.title}</td>
           <td>${post.date}</td>
---
+++ js/view.js
@@ -0,0 +1,4 @@
+getPost()
+function getPost() {
+
+}
